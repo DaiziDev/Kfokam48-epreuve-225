2e3282cc50d40c206a29d982811622cf4aad3f7c
Dazi Dev <shadowapex308@gmail.com>
2026-09-25 11:44:41 +0100

    chore: initialisation des squelettes backend et frontend

---
+++ backend/src/main/java/com/kfokam48/kfokam48/Kfokam48Application.java
@@ -0,0 +1,13 @@
+package com.kfokam48.kfokam48;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kfokam48Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kfokam48Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kfokam48/kfokam48/Kfokam48ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfokam48.kfokam48;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kfokam48ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
